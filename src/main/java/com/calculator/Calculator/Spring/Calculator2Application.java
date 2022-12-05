26ac3f198c2c48c8ed71b9f38604cf71272ccf22
Frode Lien <79.frode@gmail.com>
2022-12-05 10:42:17 +0100

    initial commit

---
+++ src/main/java/com/calculator/Calculator/Spring/Calculator2Application.java
@@ -0,0 +1,14 @@
+package com.calculator.Calculator.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator2Application.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/calculator/Calculator/Spring/Calculator2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculator.Calculator.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
